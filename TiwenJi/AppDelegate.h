1569bc691cb40a0e50abd90fe1d280f2fce66bd1
莫福见 <mofujian@danwenhaodemacbook-air.local>
2016-03-09 09:47:31 +0800

    Initial Commit

---
+++ TiwenJi/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TiwenJi
+//
+//  Created by 莫福见 on 16/3/9.
+//  Copyright © 2016年 Manridy.Bobo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
